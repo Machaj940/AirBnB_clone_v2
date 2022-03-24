05a9a9b1a5f463683ed64ea3c853f18adc87f313
John Macharia <machariajohn23@gmail.com>
2022-03-24 10:00:45 -0700

    This is the first code for BaseModel befor we add *args and **kwargs

--- models/original_base_model.py
+++ models/original_base_model.py
@@ -16,6 +16,7 @@ class BaseModel:
     def __str__(self):
         '''Return string represtentation of basemodel'''
         return "[" + type(self).__name__ + "]" "(" + self.id + ")" + str(self.__dict__)
+    # or return "return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         '''updates the public instance attribute updated_at with the current datetime'''
