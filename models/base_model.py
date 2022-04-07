b31121cec1f8693e2f98f0618230c4fbf7957653
John Macharia <machariajohn23@gmail.com>
2022-04-07 06:07:49 -0700

    Write a class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
     '''defines all common attributes/methods for other classes'''
     def __init__(self):
@@ -16,17 +15,15 @@ class BaseModel:
 
     def __str__(self):
         '''Return string represtentation of basemodel'''
-        return "[" + type(self).__name__ + "]" "(" + self.id + ")" \
-            + str(self.__dict__)
+        return "[" + type(self).__name__ + "]" "(" + self.id + ")" + str(self.__dict__)
+    # or return "return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        '''updates the public instance attribute updated_at with
-        current datetime'''
-        return self.updated_at
+        '''updates the public instance attribute updated_at with the current datetime'''
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        '''returns a dict containing all keys/values of __dict__
-        of the instance'''
+        '''returns a dictionary containing all keys/values of __dict__ of the instance'''
         cp_dct = dict(self.__dict__)
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
