5d1e72e9b07c6ec2305c858e5cb95047cebd4845
John Macharia <machariajohn23@gmail.com>
2022-04-07 12:22:01 -0700

    Write a class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances

--- models/updated_2_base_model.py
+++ models/updated_2_base_model.py
@@ -6,6 +6,7 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     '''defines all common attributes/methods for other classes'''
     def __init__(self, *args, **kwargs):
