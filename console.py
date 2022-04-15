6caad732fc6c69b473f952d71570eca4937fd426
John Macharia <machariajohn23@gmail.com>
2022-04-15 13:46:15 -0700

    Write a program called console.py that contains the entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''Write a program called console.py that contains
+the entry point of the command interpreter'''
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    '''contains the entry point of the command interpreter'''
+    PROMPT = '(hbnb) '
+
+    def do_EOF(self, line):
+        '''End Of File'''
+        return True
+
+    def do_quit(self, args):
+        '''Quit command to exit the program'''
+        raise SystemExit
+
+    def emptyline(self):
+        pass
+
+    @property
+    def prompt(self) -> str:
+        return self.PROMPT
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
