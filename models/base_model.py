29eb72c20687f333980843bbab8a3d6bfc8e1276
John Macharia <machariajohn23@gmail.com>
2023-04-06 18:07:08 +0300

    Contains the base class for our airbnb project

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,33 @@
 #!/usr/bin/python3
-'''class BaseModel defines all common attributes/methods for other classes'''
+"""This module contains the BaseModel class for our Airbnb project"""
 
 
+import datetime
 import uuid
-from datetime import datetime
-import models
 
 
 class BaseModel:
-    '''defines all common attributes/methods for other classes'''
-    def __init__(self, *args, **kwargs):
-        '''
-            Initialize public instance attributes.
-        '''
-        if (len(kwargs) == 0):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
+    """BaseModel class for our Airbnb project"""
+    def __init__(self):
+        """BaseModel initialization"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        '''Return string represtentation of basemodel'''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
-    # or return "return ("[{}] ({}) {}".format(self.__class__.__name__,
-    # self.id, self.__dict__))
-
-    def __repr__(self):
-        '''
-            Return string representation of BaseModel class
-        '''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+        """How BaseModel shoul be printed"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        '''updates the public instance attribute updated_at
-with the current datetime'''
-        self.updated_at = datetime.now()
-        models.storage.save()
+        """updates the instance attr updated_at with the current datetime"""
+        return self.updated_at
 
     def to_dict(self):
-        '''returns a dictionary containing all keys/values
-of __dict__ of the instance'''
-        cp_dct = dict(self.__dict__)
-        cp_dct['__class__'] = self.__class__.__name__
-        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        """returns a dict containing all keys/values of __dict__
+           of the instance
+        """
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
 
-        return (cp_dct)
+        self.__dict__["__class__"] = self.__class__.__name__
+        return self.__dict__
