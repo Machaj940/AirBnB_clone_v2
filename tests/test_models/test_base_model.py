ca87076ce3047cbedab5a7adb8eacfa6eac70004
John Macharia <machariajohn23@gmail.com>
2023-04-06 19:10:29 +0300

    Unittests for base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,6 +9,15 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """Test BaseModel class"""
+    def test_uuid(self):
+        """test initialization of class"""
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertIsInstance(bm1, BaseModel)
+        self.assertTrue(hasattr(bm1, "id"))
+        self.assertNotEqual(bm1.id, bm2.id)
+        self.assertIsInstance(bm1.id, str)
+
     def test_isoformat(self):
         """test isoformat of datettime"""
         now = datetime.datetime(2023, 4, 6, 18, 25, 31, 92221)
