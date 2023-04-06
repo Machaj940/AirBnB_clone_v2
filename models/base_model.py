b865d42e28a84aaffa887b4dc7d0f437abdddb5c
John Macharia <machariajohn23@gmail.com>
2023-04-06 18:45:26 +0300

    Contains the base class for our airbnb project

--- models/base_model.py
+++ models/base_model.py
@@ -20,14 +20,20 @@ class BaseModel:
 
     def save(self):
         """updates the instance attr updated_at with the current datetime"""
-        return self.updated_at
+        #return self.updated_at
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """returns a dict containing all keys/values of __dict__
            of the instance
         """
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
+        #self.created_at = self.created_at.isoformat()
+        #self.updated_at = self.updated_at.isoformat()
 
-        self.__dict__["__class__"] = self.__class__.__name__
-        return self.__dict__
+        #self.__dict__["__class__"] = self.__class__.__name__
+        #return self.__dict__
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
