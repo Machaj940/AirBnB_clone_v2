221df6d943c419bb70b928199bdb2c5fd855c0f4
John Macharia <machariajohn23@gmail.com>
2023-04-13 15:02:46 +0300

    re-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
     """BaseModel class for our Airbnb project"""
     def __init__(self, *args, **kwargs):
         """BaseModel initialization with args and kwargs"""
-        if kwargs is not None and kwargs != {}:
+        if kwargs:
             for key in kwargs:
                 if key == "created_at":
                     self.__dict__["created_at"] = datetime.datetime.strptime(
@@ -37,10 +37,10 @@ class BaseModel:
         """returns a dict containing all keys/values of __dict__
            of the instance
         """
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-
-        self.__dict__["__class__"] = self.__class__.__name__
-        return self.__dict__
+        cp_dct = dict(self.__dict__)
+        cp_dct["created_at"] = self.created_at.isoformat()
+        cp_dct["updated_at"] = self.updated_at.isoformat()
+        cp_dct["__class__"] = self.__class__.__name__
+        return cp_dct
 
 # self.__class__.__name__ is equivalent to type(self).__name__
