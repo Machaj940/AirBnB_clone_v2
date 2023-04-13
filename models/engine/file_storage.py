4006f697488b35b3087b79d8b228a80cb8d96d17
John Macharia <machariajohn23@gmail.com>
2023-04-13 15:40:29 +0300

    Serializes instances to a JSON file and deserializes JSON file to instances

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,26 +20,17 @@ class FileStorage:
     def new(self, obj):
         '''sets in __objects the obj with key <obj class name>.id'''
         key = str(obj.__class__.__name__) + "." + str(obj.id)
-        value_dict = obj
-        FileStorage.__objects[key] = value_dict
+        FileStorage.__objects[key] = obj
 
     def save(self):
         '''serializes __objects to the JSON file (path: __file_path)'''
-        objects_dict = {}
-        for key, val in FileStorage.__objects.items():
-            objects_dict[key] = val.to_dict()
-
-        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as fd:
-            json.dump(objects_dict, fd)
+        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as f:
+            json.dump(FileStorage.__objects, f, indent=4)
 
     def reload(self):
         '''Deserializes the JSON file to __objects'''
         try:
-            with open(FileStorage.__file_path, encoding="UTF8") as fd:
-                FileStorage.__objects = json.load(fd)
-            for key, val in FileStorage.__objects.items():
-                class_name = val["__class__"]
-                class_name = models.classes[class_name]
-                FileStorage.__objects[key] = class_name(**val)
+            with open(FileStorage.__file_path, encoding="UTF8") as f:
+                FileStorage.__objects = json.load(f)
         except FileNotFoundError:
             pass
