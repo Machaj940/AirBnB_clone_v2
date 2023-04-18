d14364fbdd546412bdd40d5ae3e60ccabeb49a62
John Macharia <machariajohn23@gmail.com>
2023-04-18 20:23:33 +0300

    testing sth

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,62 @@
 #!/usr/bin/python3
-"""This module contains the BaseModel class for our Airbnb project"""
-
-
-from datetime import datetime
+'''
+    This module defines the BaseModel class
+'''
 import uuid
-from models import storage
+from datetime import datetime
+import models
 
 
 class BaseModel:
-    """BaseModel class for our Airbnb project"""
+    '''
+        Base class for other classes to be used for the duration.
+    '''
     def __init__(self, *args, **kwargs):
-        """BaseModel initialization with args and kwargs"""
-        if kwargs:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
-        else:
+        '''
+            Initialize public instance attributes.
+        '''
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
-        """How BaseModel shoul be printed"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        '''
+            Return string representation of BaseModel class
+        '''
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def __repr__(self):
+        '''
+            Return string representation of BaseModel class
+        '''
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def save(self):
-        """updates the instance attr updated_at with the current datetime"""
+        '''
+            Update the updated_at attribute with new.
+        '''
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dict containing all keys/values of __dict__
-           of the instance
-        """
+        '''
+            Return dictionary representation of BaseModel class.
+        '''
         cp_dct = dict(self.__dict__)
-        cp_dct["__class__"] = self.__class__.__name__
-        cp_dct["updated_at"] = self.updated_at.isoformat()
-        cp_dct["created_at"] = self.created_at.isoformat()
-        return cp_dct
+        cp_dct['__class__'] = self.__class__.__name__
+        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
-# self.__class__.__name__ is equivalent to type(self).__name__
+        return (cp_dct)
