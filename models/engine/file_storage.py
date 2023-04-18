944fb14d8f723ade2a40928e84f7aa2df117e124
John Macharia <machariajohn23@gmail.com>
2023-04-18 20:29:21 +0300

    testing sth

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,37 +1,28 @@
-#!/usr/bin/python3
-'''
-    Define class FileStorage
-'''
 import json
-import models
 
 
 class FileStorage:
     '''
-        Serializes instances to JSON file and deserializes to JSON file.
+        Serializes instances to JSON file and deserializes to JSON file
+        to instances.
     '''
     __file_path = "file.json"
     __objects = {}
-
     def all(self):
         '''
-            Return the dictionary
+            return the dictionary
         '''
-        return self.__objects
-
+        return FileStorage.__objects
     def new(self, obj):
         '''
-            Set in __objects the obj with key <obj class name>.id
-            Aguments:
-                obj : An instance object.
+            set in __objects the obj with key <obj class name>.id
         '''
         key = str(obj.__class__.__name__) + "." + str(obj.id)
         value_dict = obj
         FileStorage.__objects[key] = value_dict
-
     def save(self):
         '''
-            Serializes __objects attribute to JSON file.
+            serializes __objects attribute to JSON file.
         '''
         objects_dict = {}
         for key, val in FileStorage.__objects.items():
@@ -42,14 +33,13 @@ class FileStorage:
 
     def reload(self):
         '''
-            Deserializes the JSON file to __objects.
+            deserializes the JSON file to __objects.
         '''
+        from models.base_model import BaseModel
         try:
             with open(FileStorage.__file_path, encoding="UTF8") as fd:
                 FileStorage.__objects = json.load(fd)
             for key, val in FileStorage.__objects.items():
-                class_name = val["__class__"]
-                class_name = models.classes[class_name]
-                FileStorage.__objects[key] = class_name(**val)
+                FileStorage.__objects[key] = BaseModel(**val)
         except FileNotFoundError:
             pass
