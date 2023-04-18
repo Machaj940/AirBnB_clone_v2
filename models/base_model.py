311aba05aa0c3cd20dffc696f0fcd56ba8482991
John Macharia <machariajohn23@gmail.com>
2023-04-18 20:41:18 +0300

    Final BaseModel class before creating the Airbnb console

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,49 @@
 #!/usr/bin/python3
-'''
-    This module defines the BaseModel class
-'''
-import uuid
+"""This module contains the BaseModel class for our Airbnb project"""
+
+
 from datetime import datetime
+import uuid
 from models import storage
+
+
 class BaseModel:
-    '''
-        Base class for other classes to be used for the duration
-        of this project.
-    '''
+    """BaseModel class for our Airbnb project"""
     def __init__(self, *args, **kwargs):
-        '''
-            Initialize public instance attributes.
-            Attributes:
-                id: id of an instance
-                created_at: Time of instance creation
-                updated_at: Time of instance update
-        '''
-        if (len(kwargs) == 0):
+        """BaseModel initialization with args and kwargs"""
+        if kwargs:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-        else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
+
     def __str__(self):
-        '''
-            Return string representation of BaseModel class
-        '''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+        """How BaseModel shoul be printed"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        '''
-            Update the updated_at attribute with new
-            current time.
-        '''
+        """updates the instance attr updated_at with the current datetime"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        '''
-            Return dictionary representation of BaseModel class.
-            convert created_at and updated_at atributes as
-            string object in ISO format.
-        '''
+        """returns a dict containing all keys/values of __dict__
+           of the instance
+        """
         cp_dct = dict(self.__dict__)
-        cp_dct['__class__'] = self.__class__.__name__
-        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        return (cp_dct)
+        cp_dct["__class__"] = self.__class__.__name__
+        cp_dct["updated_at"] = self.updated_at.isoformat()
+        cp_dct["created_at"] = self.created_at.isoformat()
+        return cp_dct
+
+# self.__class__.__name__ is equivalent to type(self).__name__
