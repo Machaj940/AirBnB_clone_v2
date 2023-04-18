416e3d6460a3b799daf380186206ba5426c9248b
John Macharia <machariajohn23@gmail.com>
2023-04-18 20:43:20 +0300

    Final FileStorage class before creating the Airbnb Console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+'''
+    Define class FileStorage
+'''
 import json
 
 
@@ -8,22 +12,19 @@ class FileStorage:
     '''
     __file_path = "file.json"
     __objects = {}
+
     def all(self):
-        '''
-            return the dictionary
-        '''
+        '''return the dictionary'''
         return FileStorage.__objects
+
     def new(self, obj):
-        '''
-            set in __objects the obj with key <obj class name>.id
-        '''
+        '''set in __objects the obj with key <obj class name>.id'''
         key = str(obj.__class__.__name__) + "." + str(obj.id)
         value_dict = obj
         FileStorage.__objects[key] = value_dict
+
     def save(self):
-        '''
-            serializes __objects attribute to JSON file.
-        '''
+        '''serializes __objects attribute to JSON file.'''
         objects_dict = {}
         for key, val in FileStorage.__objects.items():
             objects_dict[key] = val.to_dict()
@@ -32,10 +33,9 @@ class FileStorage:
             json.dump(objects_dict, fd)
 
     def reload(self):
-        '''
-            deserializes the JSON file to __objects.
-        '''
+        '''deserializes the JSON file to __objects.'''
         from models.base_model import BaseModel
+
         try:
             with open(FileStorage.__file_path, encoding="UTF8") as fd:
                 FileStorage.__objects = json.load(fd)
