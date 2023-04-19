2af258695e2c851283472d08ff3cfae210b4456f
John Macharia <machariajohn23@gmail.com>
2023-04-19 21:24:06 +0300

    Add User class that contains names, email, password of user

--- models/user.py
+++ models/user.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-'''
-    Implementation of the User class which inherits from BaseModel
-'''
+"""Class User inherits from BaseModel"""
+
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    '''
-        Definition of the User class
-    '''
+    """the User class"""
     email = ""
     password = ""
     first_name = ""
