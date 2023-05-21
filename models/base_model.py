c1edebf04c81e515f667a79c1e297d25f829393e
John Macharia <machariajohn23@gmail.com>
2023-05-21 22:35:55 +0300

    Change the initialization of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,32 +4,26 @@
 
 from datetime import datetime
 import uuid
-from models import storage
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-
-
-Base = declarative_base()
+import models
 
 
 class BaseModel:
     """BaseModel class for our Airbnb project"""
-    id = Column(String(60), primary_key=True, nullable=False, unique=True)
-    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-
     def __init__(self, *args, **kwargs):
         """BaseModel initialization with args and kwargs"""
-        if not kwargs:
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
-            for k in kwargs:
-                if k in ['created_at', 'updated_at']:
-                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
-                elif k != '__class__':
-                    setattr(self, k, kwargs[k])
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """How BaseModel shoul be printed"""
@@ -43,28 +37,14 @@ class BaseModel:
     def save(self):
         """updates the instance attr updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dict containing all keys/values of __dict__
            of the instance
         """
         cp_dct = dict(self.__dict__)
-        try:
-            del cp_dct['_sa_instance_state']
-        except ValueError:
-            pass
         cp_dct["__class__"] = self.__class__.__name__
         cp_dct["updated_at"] = self.updated_at.isoformat()
         cp_dct["created_at"] = self.created_at.isoformat()
         return cp_dct
-
-    def delete(self):
-        '''
-            Deletes the current instance from the storage
-                by calling the method delete.
-        '''
-        storage.delete(self)
-
-# self.__class__.__name__ is equivalent to type(self).__name__
