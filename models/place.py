ec3228905e1526046d753456c4e401943121a2b8
John Macharia <machariajohn23@gmail.com>
2023-05-26 20:13:33 +0300

    Define the class with both db_storage and filestorage

--- models/place.py
+++ models/place.py
@@ -5,15 +5,14 @@
 import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
-from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
     """State class"""
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "places"
-        city_id = Column(String(60), nullable=False, ForeignKey('cities.id'))
-        user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024))
         number_rooms = Column(Integer, default=0, nullable=False)
@@ -25,13 +24,13 @@ class Place(BaseModel, Base):
         amenity_ids = []
     else:
         city_id = ""
-	user_id = ""
+        user_id = ""
         name = ""
         description = ""
         number_rooms = 0
         number_bathrooms = 0
-	max_guest = 0
-	price_by_night = 0
+        max_guest = 0
+        price_by_night = 0
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
