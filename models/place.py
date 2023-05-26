e84e9674e7099fb16787455d1d5b4bd0a80f33d1
John Macharia <machariajohn23@gmail.com>
2023-05-26 21:44:41 +0300

    Update Place class with a Table called place_amenity for creating the relationship Many-To-Many between Place and Amenity

--- models/place.py
+++ models/place.py
@@ -15,11 +15,9 @@ if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         association_table = Table('place_amenity', Base.metadata,
                                   Column('place_id', String(60),
                                          ForeignKey('places.id'),
-                                         primary_key=True,
                                          nullable=False),
                                   Column('amenity_id', String(60),
                                          ForeignKey('amenities.id'),
-                                         primary_key=True,
                                          nullable=False))
 
 
