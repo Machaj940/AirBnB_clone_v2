516791e282ca75d56276b5b9f19538884757db0a
John Macharia <machariajohn23@gmail.com>
2023-05-26 20:44:20 +0300

    Define the class with both db_storage and filestorage

--- models/review.py
+++ models/review.py
@@ -2,11 +2,19 @@
 """contains the review class"""
 
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import os
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """class Review"""
-    place_id = ""
-    user_id = ""
-    text = ""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
