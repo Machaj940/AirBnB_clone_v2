dadab76c1997f2bc6d4abbb9b93601a80e38c65c
John Macharia <machariajohn23@gmail.com>
2023-05-30 17:10:41 +0300

    Add an instance of SQLAlchemy Table called place_amenity for creating the relationship Many-To-Many between Place and Amenity

--- models/place.py
+++ models/place.py
@@ -10,7 +10,8 @@ from sqlalchemy.orm import relationship
 
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
+    metadata = Base.metadata
+    place_amenity = Table('place_amenity', metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id'), primary_key=True,
                                  nullable=False),
