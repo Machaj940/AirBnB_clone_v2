06c711fd7b7e673d40aa9cdbccc943b507c1c814
John Macharia <machariajohn23@gmail.com>
2023-05-30 16:47:28 +0300

    Has a many-to-many relationship with Place class

--- models/amenity.py
+++ models/amenity.py
@@ -4,8 +4,8 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-import os
 from sqlalchemy.orm import relationship
+import os
 
 
 class Amenity(BaseModel, Base):
