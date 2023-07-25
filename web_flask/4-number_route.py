bca651158d9d56fd96a26d07091ef53748476b7c
John Macharia <machariajohn23@gmail.com>
2023-07-25 22:42:06 +0300

    display route if the variable given is a number only

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''script that starts a Flask web app'''
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    ''' return Hello HBNB! '''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' return HBNB '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hbnb_text(text):
+    ''' return HBNB '''
+    return "C %s" % text.replace(r'_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hbnb_text_default(text="is cool"):
+    ''' return HBNB '''
+    return "Python %s" % text.replace(r'_', ' ')
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    ''' return HBNB '''
+    return "%i is a number" % int(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
