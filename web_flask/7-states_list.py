732496ce13d520a11f64acadee58ce5192247836
John Macharia <machariajohn23@gmail.com>
2023-07-28 22:06:17 +0300

    Flask web app for fetching data from the storage engine

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''script that starts a Flask web app'''
+
+
+from flask import Flask, render_template
+from models import storage, State
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown(exceptions):
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    state_obj = storage.all("State")
+    states = list()
+    for state, value in state_obj.items():
+        states.append(value)
+    return render_template("7-states_list.html", states=states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
