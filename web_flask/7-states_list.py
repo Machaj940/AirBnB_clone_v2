f8f9f5aa653d724ba66c6856f9e2d5123ea9abf2
John Macharia <machariajohn23@gmail.com>
2023-07-28 22:08:58 +0300

    Flask web app for fetching data from the storage engine

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template
 from models import storage, State
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown(exceptions):
     storage.close()
@@ -18,6 +19,7 @@ def states_list():
     for state, value in state_obj.items():
         states.append(value)
     return render_template("7-states_list.html", states=states)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
